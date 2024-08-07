e0b9321a3f29d44cdbcb774cc6501f6d635be990
Xuho <you@example.com>
2024-08-07 23:15:33 +0700

    update

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculator
 {
@@ -10,43 +6,70 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            int num1;
-            int num2;
-            int result;
-
+            float num1;
+            float num2;
+            float result;
             string answer;
-            Console.WriteLine("Hello, welcome to the calculate program!");
-            Console.WriteLine("Please enter your first number.");
 
-            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, welcome to the calculator program!");
 
-            Console.WriteLine("Please enter your second number.");
-
-            num2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("What type of operation would you like to do?");
-            Console.WriteLine("Please enter a for addition, s for substraction, m for multiplication or other key for division.");
-
-            answer = Console.ReadLine();
-
-            if (answer =="a")
+            while (true)
             {
-                result = num1 + num2;
-            }
-            else if (answer == "s")
-            {
-                result = num1 - num2;
-            }
-            else if (answer == "m")
-            {
-                result = num1 * num2;
-            }
-            else result = num1 / num2;
+                Console.WriteLine("Please enter your first number:");
+                if (!float.TryParse(Console.ReadLine(), out num1))
+                {
+                    Console.WriteLine("Invalid input, please enter a valid number.");
+                    continue;
+                }
 
-            Console.WriteLine("The reult is: " + result);
-            Console.WriteLine("Thank you for using the calculator program!");
+                Console.WriteLine("Please enter your second number:");
+                if (!float.TryParse(Console.ReadLine(), out num2))
+                {
+                    Console.WriteLine("Invalid input, please enter a valid number.");
+                    continue;
+                }
+
+                Console.WriteLine("What type of operation would you like to do?");
+                Console.WriteLine("Please enter 'a' for addition, 's' for subtraction, 'm' for multiplication, 'd' for division, or 'x' to exit.");
+
+                answer = Console.ReadLine();
+
+                if (answer == "x")
+                {
+                    Console.WriteLine("Thank you for using the calculator program!");
+                    break;
+                }
+
+                switch (answer)
+                {
+                    case "a":
+                        result = num1 + num2;
+                        break;
+                    case "s":
+                        result = num1 - num2;
+                        break;
+                    case "m":
+                        result = num1 * num2;
+                        break;
+                    case "d":
+                        result = num2 == 0 ? float.NaN : num1 / num2;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation selected. Please try again.");
+                        continue;
+                }
+
+                if (float.IsNaN(result))
+                {
+                    Console.WriteLine("Cannot divide by 0.");
+                }
+                else
+                {
+                    Console.WriteLine("The result is: " + result);
+                }
+            }
+
             Console.ReadKey();
-
         }
     }
 }
